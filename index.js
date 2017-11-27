0c7a6b2e62d6d13816b613d89290a8441a8cf075
Jose Antonio Santiago <joselondres@hotmail.com>
2017-11-27 15:56:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 // write your code below!
 
 var name = "Janet"
-var height = "74"
+var height = 74
 
 console.log("Name:")
 console.log(name)
 
-height = parseInt(height);
+
 console.log("Height:")
 console.log(height +1)
